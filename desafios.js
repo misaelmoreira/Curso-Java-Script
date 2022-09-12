fb9cca4a4480a61a0575c2c71d250aea934d38fc
misaelmoreira <misael.alberto@outlook.com>
2022-09-12 08:41:02 -0300

    Adicionando git Ignore

---
+++ desafios.js
@@ -0,0 +1,13 @@
+var frase = '';
+let N = 4;
+
+for ( i = 1; i <= N ; i++) {
+    if (i < N ) {      
+      frase += 'Ho ';      
+    } 
+    else {
+      frase += 'Ho!';
+    }     
+}
+
+console.log(frase);
